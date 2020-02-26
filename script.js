d94f42ccf6f805169c638611706ac336e78aa788
Anne Gry Omholt <zapp87@hotmail.com>
2020-02-26 22:40:12 +0100

    tredje

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // JavaScript Document
+fetch("https://api.spacexdata.com/v3/launches/latest")
+	.then(function(response) {
+	return response.json();
+})
+	.then(function(json) {
+	loopTroughResults(json);
+})
+	.catch(function(error){
+	console.log(error);
+});

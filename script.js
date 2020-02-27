08e29dc33b2dc04a3b31dbbd1fa48bbea328a77b
Anne Gry Omholt <zapp87@hotmail.com>
2020-02-27 18:44:07 +0100

    initial commit

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 // JavaScript Document
-fetch("https://api.spacexdata.com/v3/launches/latest")
-	.then(function(response) {
+
+
+fetch("https://api.spacexdata.com/v3/info")
+	.then(function(response){
 	return response.json();
-})
-	.then(function(json) {
-	loopTroughResults(json);
-})
+	})
+	.then(function(json){
+	newInfo(json);
+	})
+
 	.catch(function(error){
-	console.log(error);
+	console.dir(error);
 });
+
+	
+	function newInfo(info){
+		console.dir(info);
+		
+		var infoSection = document.getElementById("content");
+			infoSection.innerHTML +=
+			`<div>
+				<p>Name: ${info.name}</p>
+				<p>Founder: ${info.founder}</p>
+				<p>Founded: ${info.founded}</p>
+				<p>Employees: ${info.employees}</p>
+				<p>Vehicles: ${info.vehicles}</p>
+				<p>Launch Sites: ${info.launch_sites}</p>
+			</div>`
+		}
+	
+
+
